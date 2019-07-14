bd98a9ce95985873d1ecb0ba581fa930130ea1d3
moharick <moharick@gmail.com>
2019-07-14 21:23:06 +0300

    Add first function.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@ var century, year, month, dayOfMonth, dayOfBirth, day, gender, genderval;
 var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
 var DD = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
+
+function myInput() {
+    century = parseInt(document.getElementById("century").value);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    dayOfMonth = parseInt(document.getElementById("day-of-month").value);
+}
