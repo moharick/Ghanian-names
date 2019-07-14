4874824d6aef02554c861664c44c106c1d118c6d
moharick <moharick@gmail.com>
2019-07-14 21:26:26 +0300

    Add a function.

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,47 @@ function check() {
     } else {
         // console.log("radios are check");
     }
+    switch (gender) {
+        case gender = "male":
+            if (day == 0) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[0] + " Your name is " + maleName[0];
+            } else if (day == 1) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[1] + " Your name is " + maleName[1];
+            } else if (day == 2) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[2] + " Your name is " + maleName[2];
+            } else if (day == 3) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[3] + " Your name is " + maleName[3];
+            } else if (day == 4) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[4] + " Your name is " + maleName[4];
+            } else if (day == 5) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[5] + " Your name is " + maleName[5];
+            } else if (day == 6) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[6] + " Your name is " + maleName[6];
+            } else {
+                // console.log("pass");
+            }
+            break;
+        case gender = "female":
+            if (day == 0) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[0] + " Your name is " + femaleName[0];
+            } else if (day == 1) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[1] + " Your name is " + femaleName[1];
+            } else if (day == 2) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[2] + " Your name is " + femaleName[2];
+            } else if (day == 3) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[3] + " Your name is " + femaleName[3];
+            } else if (day == 4) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[4] + " Your name is " + femaleName[4];
+            } else if (day == 5) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[5] + " Your name is " + femaleName[5];
+            } else if (day == 6) {
+                document.getElementById("result").innerHTML = " Since you were born on " + DD[6] + " Your name is " + femaleName[6];
+            } else {
+                // alert("pass");
+            }
+            break;
+        default:
+            // console.log("go");
+
+    }
+}
